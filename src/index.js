6d30d6bf7f54ccdd749e3faedc130b13a64659fd
Adam <adamh93@verizon.net>
2018-04-19 22:13:07 -0400

    done: Base templates; tasks: Create NavBar Component, Create Slide show component

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import
+
+export default class Slider extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div className="slider">
+
+      </div>
+    );
+  }
+}
