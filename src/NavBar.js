30acc3673b68277dd4b65ae732bd8e9963d17c11
Adam <adamh93@verizon.net>
2018-04-25 22:18:02 -0400

    done: Added test image to index.js, modify nav bar menu styling to horizontal

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './index.css'
 
 export class NavBar extends React.Component{
 
   render(){
     return(
-      <div>
-        <h3><a>Home</a></h3>
-        <h3><a>Next</a></h3>
-        <h3><a>Prev</a></h3>
+      <div className="nav-menu">
+        <h3 className="nav-menu"><a>Home</a></h3>
+        <h3 className="nav-menu"><a>Next</a></h3>
+        <h3 className="nav-menu"><a>Prev</a></h3>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { NavBar } from './NavBar.js'
+import image from './img/image1.jpg';
 
 export default class Slider extends React.Component {
   constructor(props){
     super(props);
 
     this.state = {}
+
   }
 
   render() {
+    this.slides = ['image-1.png', 'image-2.png', 'image-3.png', 'image-4.png'];
+
     return(
       <div>
         <NavBar />
         <div className="slider">
-
+          <h3>Left</h3>
+          <img src={image} />
+          <h3>Right</h3>
         </div>
       </div>
     );
