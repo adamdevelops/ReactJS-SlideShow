25919e23ffafc81ab8dbb62636585829837d1738
Adam <adamh93@verizon.net>
2018-04-26 15:09:42 -0400

    done: Created slide pics array to hold pics, import two pics into index.js, Added slide number to state, create constant var slideNum to hold slide number held the state property, use the constant slideNum to iterate image in the this.slides array, added test heading iterating const SlideNum to display its value

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import './index.css'
 import { NavBar } from './NavBar.js'
 import image from './img/image1.jpg';
+import image2 from './img/image-2.png';
+
 
 export default class Slider extends React.Component {
   constructor(props){
     super(props);
 
-    this.state = {}
+    this.state = {slideNum: 0};
 
   }
 
   render() {
-    this.slides = ['image-1.png', 'image-2.png', 'image-3.png', 'image-4.png'];
+    this.slides = [ image, image2, 'image-3.png', 'image-4.png'];
+    const slideNum = this.state.slideNum;
 
     return(
       <div>
         <NavBar />
         <div className="slider">
           <h3>Left</h3>
-          <img src={image} />
+          <h3>{slideNum}</h3>
+          <img src={this.slides[slideNum]} />
           <h3>Right</h3>
         </div>
       </div>
