f34017461594d3e7fdb1a2a1399578d8fd95456d
Adam <adamh93@verizon.net>
2018-04-29 21:28:29 -0400

    fix: Fixed undefined state value by refactor incrementSlideNum() func to use an arrow function instead

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ export class NavBar extends React.Component{
 
   render(){
     return(
-      <div className="nav-menu">
+      <div className="">
         <h3 className="nav-menu"><a>Home</a></h3>
         <h3 className="nav-menu"><a>Next</a></h3>
         <h3 className="nav-menu"><a>Prev</a></h3>
--- src/index.js
+++ src/index.js
@@ -14,18 +14,27 @@ export default class Slider extends React.Component {
 
   }
 
+  incrementSlideNum = () => {
+    console.log(this.state.slideNum);
+    let slideNum = this.state.slideNum;
+
+    this.setState({ slideNum: slideNum + 1});
+  }
+
+
+
   render() {
-    this.slides = [ image, image2, 'image-3.png', 'image-4.png'];
+    this.slides = [ image, image2, 'image-3.png', 'image-4.png'];  // add more pics to slider
     const slideNum = this.state.slideNum;
 
     return(
       <div>
         <NavBar />
         <div className="slider">
-          <h3>Left</h3>
           <h3>{slideNum}</h3>
+          <h3>Left</h3>
           <img src={this.slides[slideNum]} />
-          <h3>Right</h3>
+          <h3><a onClick={this.incrementSlideNum}>Right</a></h3>
         </div>
       </div>
     );
